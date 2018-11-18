bb58cb290749a0697bffceddde6be2dd20492a2e
Swizec Teller <swizec@swizec.com>
2018-11-17 21:09:56 -0800

    Works with multiple tweets

--- src/showdown/TwitterScreenshot.js
+++ src/showdown/TwitterScreenshot.js
@@ -3,7 +3,7 @@ import React from "react";
 export const extension = function() {
     const twitter = {
         type: "lang",
-        regex: /\[((http|https):\/\/(www){0,1}twitter.com\/(.+))\]/,
+        regex: /\[((http|https):\/\/(www){0,1}twitter.com\/(.+))\]/g,
         replace: function(match, url) {
             return `<TwitterScreenshot tweet="${url}" />`;
         }
@@ -20,12 +20,16 @@ export class Component extends React.Component {
     async componentDidMount() {
         const { tweet } = this.props;
 
-        const res = await fetch(
-                `https://84wz7ux5rc.execute-api.us-east-1.amazonaws.com/default/screenshot-as-a-service-dev-screenshot-function?url=${tweet}`
-            ),
-            url = await res.text();
+        try {
+            const res = await fetch(
+                    `https://84wz7ux5rc.execute-api.us-east-1.amazonaws.com/default/screenshot-as-a-service-dev-screenshot-function?url=${tweet}`
+                ),
+                url = await res.text();
 
-        this.setState({ image: url });
+            this.setState({ image: url });
+        } catch (e) {
+            this.setState({ image: "/undraw_warning_cyit.png" });
+        }
     }
 
     render() {
