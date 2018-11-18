a29ad26ae37e5fc3b30581c0f0074cd4f5d0ad78
Swizec Teller <swizec@swizec.com>
2018-11-17 20:51:24 -0800

    Tweets turn into screenshots

--- src/LetterRender.js
+++ src/LetterRender.js
@@ -2,25 +2,33 @@ import React from "react";
 import { Content, Heading } from "reactbulma";
 import showdown from "showdown";
 import "showdown-twitter";
+import { Markdown } from "react-showdown";
 
 import { Consumer } from "./Context";
 import TwitterScreenshot from "./showdown/TwitterScreenshot";
 
 export default () => {
-    const converter = new showdown.Converter({
-        extensions: ["twitter", TwitterScreenshot]
-    });
+    // const converter = new showdown.Converter({
+    //     extensions: ["twitter", TwitterScreenshot]
+    // });
 
     return (
         <Consumer>
             {({ markdown }) => (
                 <React.Fragment>
                     <Heading>Rendered 💌</Heading>
-                    <Content
-                        dangerouslySetInnerHTML={{
-                            __html: converter.makeHtml(markdown)
-                        }}
-                    />
+                    <Content>
+                        <Markdown
+                            markup={markdown}
+                            extensions={[
+                                "twitter",
+                                TwitterScreenshot.extension
+                            ]}
+                            components={{
+                                TwitterScreenshot: TwitterScreenshot.Component
+                            }}
+                        />
+                    </Content>
                 </React.Fragment>
             )}
         </Consumer>
--- src/showdown/TwitterScreenshot.js
+++ src/showdown/TwitterScreenshot.js
@@ -1,14 +1,47 @@
-export default function() {
+import React from "react";
+
+export const extension = function() {
     const twitter = {
         type: "lang",
         regex: /\[((http|https):\/\/(www){0,1}twitter.com\/(.+))\]/,
         replace: function(match, url) {
-            return `[![](https://media.giphy.com/media/k5lsGnQMi1HVFMNrOF/giphy.gif)](${url})`;
+            return `<TwitterScreenshot tweet="${url}" />`;
         }
     };
 
     return [twitter];
+};
+
+export class Component extends React.Component {
+    state = {
+        image: "/lg.pink-pig-ajax-loader.gif"
+    };
+
+    async componentDidMount() {
+        const { tweet } = this.props;
+
+        const res = await fetch(
+                `https://84wz7ux5rc.execute-api.us-east-1.amazonaws.com/default/screenshot-as-a-service-dev-screenshot-function?url=${tweet}`
+            ),
+            url = await res.text();
+
+        this.setState({ image: url });
+    }
+
+    render() {
+        const { image } = this.state,
+            { tweet } = this.props;
+
+        return (
+            <a href={tweet} target="_blank" rel="noopener noreferrer">
+                <img
+                    src={image}
+                    style={{ maxWidth: 480 }}
+                    alt={`Tweet from ${tweet}`}
+                />
+            </a>
+        );
+    }
 }
 
-//
-// [https://twitter.com/Swizec/status/1063925383085547520]
+export default { extension, Component };
