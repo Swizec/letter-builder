8684b2c84f7766daa7ee64bd8684af06a05110c5
Swizec Teller <swizec@swizec.com>
2019-04-07 15:40:25 -0700

    Refactor Screenshot component to hooks

--- src/Screenshot.js
+++ src/Screenshot.js
@@ -1,36 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // https://twitter.com/thepatwalls/status/1064024065961119745
 // https://www.youtube.com/watch?v=pwkYP4iFt5Y
 
-export class Component extends React.Component {
-    state = {
-        image: "/lg.pink-pig-ajax-loader.gif"
-    };
+function Screenshot({ node }) {
+    const [image, setImage] = useState("/lg.pink-pig-ajax-loader.gif");
 
-    async componentDidMount() {
-        const { node } = this.props;
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await fetch(node.url),
+                    url = await res.text();
 
-        try {
-            const res = await fetch(node.url),
-                url = await res.text();
+                setImage(url);
+            } catch (e) {
+                setImage("/undraw_warning_cyit.png");
+            }
+        })();
+    }, [node]);
 
-            this.setState({ image: url });
-        } catch (e) {
-            this.setState({ image: "/undraw_warning_cyit.png" });
-        }
-    }
-
-    render() {
-        const { image } = this.state,
-            { node } = this.props;
-
-        return (
-            <a href={image} target="_blank" rel="noopener noreferrer">
-                <img src={image} style={{ maxWidth: 480 }} alt={node.value} />
-            </a>
-        );
-    }
+    return (
+        <a href={image} target="_blank" rel="noopener noreferrer">
+            <img src={image} style={{ maxWidth: 480 }} alt={node.value} />
+        </a>
+    );
 }
 
-export default Component;
+export default Screenshot;
