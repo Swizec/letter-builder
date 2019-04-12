c25dc0f87032c280874aa84880ed05240d716cee
Swizec Teller <swizec@swizec.com>
2019-04-11 23:25:48 -0700

    Working implementation of github search through markdown

--- src/utils/remarkGithubLinks.js
+++ src/utils/remarkGithubLinks.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import visit from "unist-util-visit";
-import GitHub from "github-api";
+import Octokit from "@octokit/rest";
+
+const octokit = new Octokit({});
 
 //[remark](gh:remarkjs/remark)
 
@@ -27,34 +29,41 @@ function githubLinks() {
         });
 }
 
-const GithubLink = ({ node }) => {
-    const [url, setUrl] = useState(null);
+const GithubLink = React.memo(
+    ({ node }) => {
+        const [url, setUrl] = useState(null);
 
-    useEffect(
-        function() {
-            (async function() {
-                const gh = new GitHub();
-                const query = node.url.replace(/^gh:/, "");
-                const result = await gh
-                    .search({
-                        q: query
-                    })
-                    .forRepositories();
+        console.log("Rendering", node.url);
 
-                const url = result.data.sort((a, b) => b.score - a.score)[0]
-                    .html_url;
+        useEffect(
+            function() {
+                (async function() {
+                    const query = node.url.replace(/^gh:/, "");
 
-                setUrl(url);
-            })();
-        },
-        [node]
-    );
+                    const result = await octokit.search.repos({
+                        q: query,
+                        page: 1
+                    });
 
-    return url ? (
-        <a href={url}>{node.children[0].value}</a>
-    ) : (
-        <i>Searching {node.url}</i>
-    );
-};
+                    const url = result.data.items.sort(
+                        (a, b) => b.score - a.score
+                    )[0].html_url;
+
+                    setUrl(url);
+                })();
+            },
+            [node]
+        );
+
+        return url ? (
+            <a href={url}>{node.children[0].value}</a>
+        ) : (
+            <i>Searching {node.url}</i>
+        );
+    },
+    (prevProps, nextProps) =>
+        prevProps.node.url === nextProps.node.url &&
+        prevProps.node.children[0].value === nextProps.node.children[0].value
+);
 
 export { githubLinks, GithubLink };
