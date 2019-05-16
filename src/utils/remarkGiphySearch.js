04946245d07a3328e34d5f5c00b594d6cb645684
Swizec Teller <swizec@swizec.com>
2019-05-15 23:17:56 -0700

    Load giphys and allow scrollthrough

--- src/utils/remarkGiphySearch.js
+++ src/utils/remarkGiphySearch.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import visit from "unist-util-visit";
+import giphy from "giphy-api";
 
 // ![](giphy:hello)
 
@@ -25,9 +26,56 @@ function remarkGiphySearch() {
         });
 }
 
-const GiphySearch = React.memo(({ node }) => {
-    console.log(node);
-    return null;
-});
+const GiphySearch = React.memo(
+    ({ node }) => {
+        const [{ image }, dispatch] = useReducer(
+            function(state, action) {
+                switch (action.type) {
+                    case "foundGiphys":
+                        return {
+                            giphys: action.giphys,
+                            index: 0,
+                            image: action.giphys[0].images.downsized_medium.url
+                        };
+                    case "nextGiphy":
+                        let index = (state.index + 1) % state.giphys.length;
+                        return {
+                            ...state,
+                            index,
+                            image:
+                                state.giphys[index].images.downsized_medium.url
+                        };
+                    default:
+                        throw new Error();
+                }
+            },
+            {
+                results: [],
+                image: "/lg.pink-pig-ajax-loader.gif",
+                index: 0
+            }
+        );
+
+        useEffect(() => {
+            (async () => {
+                const results = await giphy(
+                    "BbjXTpBIYN0GwoBCRpPLUCF08EPJ6PUp"
+                ).search(node.search);
+
+                dispatch({ type: "foundGiphys", giphys: results.data });
+            })();
+        }, [node.search]);
+
+        return (
+            <img
+                src={image}
+                style={{ maxWidth: 480 }}
+                alt={`${node.search} giphy`}
+                onClick={() => dispatch({ type: "nextGiphy" })}
+            />
+        );
+    },
+    (prevProps, nextProps) => prevProps.node.search === nextProps.node.search
+);
 
 export { remarkGiphySearch, GiphySearch };
