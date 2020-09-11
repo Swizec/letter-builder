bea96aa8d724c6d44868ae3d620fee47df1f7ea1
Swizec Teller <swizec@swizec.com>
2020-09-11 08:57:34 -0700

    Fix bug in giphy search

--- src/utils/remarkGiphySearch.js
+++ src/utils/remarkGiphySearch.js
@@ -11,21 +11,23 @@ import ReloadButton from "../ReloadButton";
 // ![](giphy:rick_and_morty)
 
 function remarkGiphySearch() {
-    return tree =>
+    return (tree) =>
         new Promise(async (resolve, reject) => {
             const nodesToChange = [];
 
-            visit(tree, "image", node => {
+            visit(tree, "image", (node) => {
                 if (node.url.startsWith("giphy:")) {
                     nodesToChange.push({
-                        node
+                        node,
                     });
                 }
             });
 
             for (const { node } of nodesToChange) {
                 node.type = "giphySearch";
-                node.search = node.url.replace(/^giphy:/, "");
+                node.search = node.url
+                    .replace(/^giphy:/, "")
+                    .replace(/_/g, " ");
             }
 
             resolve();
@@ -38,14 +40,14 @@ function giphyReducer(state, action) {
             return {
                 giphys: action.giphys,
                 index: 0,
-                image: action.giphys[0].images.downsized_medium.url
+                image: action.giphys[0].images.downsized_medium.url,
             };
         case "nextGiphy":
             let index = (state.index + 1) % state.giphys.length;
             return {
                 ...state,
                 index,
-                image: state.giphys[index].images.downsized_medium.url
+                image: state.giphys[index].images.downsized_medium.url,
             };
         default:
             throw new Error();
@@ -55,23 +57,26 @@ function giphyReducer(state, action) {
 const initialState = {
     results: [],
     image: loaderImg,
-    index: 0
+    index: 0,
 };
 
 const GiphySearch = React.memo(
     ({ node }) => {
         const [{ image }, dispatch] = useReducer(giphyReducer, initialState);
 
-        useEffect(() => {
-            (async () => {
-                const results = await giphy({
-                    apiKey: giphyAPIKey,
-                    https: true
-                }).search(node.search);
+        useEffect(
+            () => {
+                (async () => {
+                    const results = await giphy({
+                        apiKey: giphyAPIKey,
+                        https: true,
+                    }).search(node.search);
 
-                dispatch({ type: "foundGiphys", giphys: results.data });
-            })();
-        }, [node.search]);
+                    dispatch({ type: "foundGiphys", giphys: results.data });
+                })();
+            },
+            [node.search]
+        );
 
         return (
             <>
